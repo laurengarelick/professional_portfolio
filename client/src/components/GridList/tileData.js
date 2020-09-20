329e45b63fcb860b89b3bbd35842d2e6aaeeabe3
lauren garelick <laurengarelick@gmail.com>
2020-09-19 23:46:23 -0700

    test

---
+++ client/src/components/GridList/tileData.js
@@ -0,0 +1,14 @@
+import image from 'client\public\images\headshot.jpeg';
+
+
+const tileData = [
+    {
+        img: client\public\images\headshot.jpeg,
+        title: 'headshot',
+        author: 'Lauren Garelick',
+        featured: true,
+    },
+    {
+        [etc...]
+   },
+  ];
