72aaec02db570d475c4ad63d0fa97b0366a8751d
lauren garelick <laurengarelick@gmail.com>
2020-09-27 23:58:43 -0700

    text

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from "./components/Nav/Nav.js";
 import GridList from "./components/GridList/GridList.js"
-import Card from "./components/Card/Card.js";
+
 
 
 
@@ -12,7 +12,8 @@ function App() {
     <div className="App">
       <Nav/>
       <div className="banner" style={{background:"#424242", height: "50vh"}}>
-        <h1 style={{textAlign:"center", color:"white", fontSize:"32px"}}>Hi, I’m a MOM, MERN coding apprentice, and adventure enthusiast living in Orange County, CA.</h1>
+        <h1 style={{textAlign:"center", padding: 100, color:"white", fontSize:"32px"}}>-Hi, I’m a MOM, MERN coding apprentice, and
+         adventure enthusiast living in Orange County, CA.</h1>
       </div>
      <GridList/>
     </div>
